85d4ef8e28d2714b18cbaa984d52a58995978a00
Roy Ratcliffe <roy@ratcliffe.me>
2026-01-25 07:55:53 +0000

    fix(exit_test): add output flushing and exit indication
    Enhance the core0_main function to write the string "EXIT\n" to
    the TriCore test device, indicating test completion. Also,
    include a magic value to flush the output buffer before
    triggering an exit with a specific exit code.

--- Tests/exit_test.c
+++ Tests/exit_test.c
@@ -1,5 +1,19 @@
 int core0_main(void) {
   /*
+   * Write the string "EXIT\n" to the TriCore test device to indicate test
+   * completion.
+   */
+  for (const char *con = "EXIT\n"; *con != '\0'; con++) {
+    *(volatile int *)0xf0000000U = 0x100 | (*con & 0xff);
+  }
+
+  /*
+   * Write the magic value to the TriCore test device to flush the output
+   * buffer.
+   */
+  *(volatile int *)0xf0000000U = 0x200;
+
+  /*
    * Write a value to the TriCore test device to trigger an exit.
    * The value written determines the exit code of QEMU.
    */
